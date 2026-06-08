c4a41461fc131bc3b87628cded583726c655e054
umalavanya <umalavanya7@gmail.com>
2026-06-08 01:04:38 -0400

    typescript practice

---
+++ UmaPractice/p1_typescript/01-helloworld/myhelloworld.ts
@@ -0,0 +1,2 @@
+console.log("Helloworld!") ;
+console.log("Coding Gurus "+ "Time for some fun!")
---
+++ UmaPractice/p1_typescript/02-variables/sample-types.js
@@ -0,0 +1,11 @@
+var found = true;
+var grade = 88.6;
+var firstname = 'Anup';
+var lastname = "Kumar";
+// let's break it! 
+found = 0;
+grade = "A";
+firstName = false;
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstname + " " + lastname);
