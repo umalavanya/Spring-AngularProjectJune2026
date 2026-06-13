8f5646b9dd4e7b60a4d42c6617175b04b9f2ac6c
umalavanya <umalavanya7@gmail.com>
2026-06-13 08:51:40 -0400

    Update sales-person-list.ts

--- angular-uma-training/sales-project/src/app/sales-person-list/sales-person-list.ts
+++ angular-uma-training/sales-project/src/app/sales-person-list/sales-person-list.ts
@@ -7,13 +7,15 @@ import { SalesPerson } from './sales-person';
   templateUrl: './sales-person-list.html',
   styleUrl: './sales-person-list.css',
 })
+
+
 export class SalesPersonList {
 
   // create an array of objects
   salesPersonList: SalesPerson[] = [
 
     new SalesPerson("Anup", "Kumar", "anup.kumar@luv2code.com", 50000),
-    
+
     new SalesPerson("Jhon", "Doe", "jdoe@luv2code.com", 40000)
     
 
